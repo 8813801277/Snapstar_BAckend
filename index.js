4e523e77d5c48959588e180d973d15aec35eace2
saini__mukul <sainimukul806@gmail.com>
2024-09-08 22:27:51 +0530

    Commited by mukul Saini

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const PORT = process.env.PORT || 4000;
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true
-};
-app.use(cors(corsOptions));
+// app.use(express.urlencoded({ extended: true }));
+// const corsOptions = {
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// };
+// app.use(cors(corsOptions));
 
 //api's
 app.use("/api/v2/user", userRoute)
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { editProfile, getProfile, login, register, logout, getSuggesstions, followOrUnfollow} from "../controllers/user.controller.js";
+import {getProfile, login, register, logout, followOrUnfollow} from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../middlewares/multer.js";
 const router = express.Router();
@@ -8,8 +8,8 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
 router.route('/:id/profile').get(isAuthenticated, getProfile);
-router.route('/profile/edit').post(isAuthenticated, upload.single('profilePicture'), editProfile);
-router.route('/suggested').get(isAuthenticated, getSuggesstions);
+// router.route('/profile/edit').post(isAuthenticated, upload.single('profilePicture'), editProfile);
+// router.route('/suggested').get(isAuthenticated, getSuggesstions);
 router.route('/followorunfollow/:id').post(isAuthenticated, followOrUnfollow);
 
 export default router;
